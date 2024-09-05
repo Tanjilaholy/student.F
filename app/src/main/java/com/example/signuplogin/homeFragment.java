7d5717017e09c5e51eca8e7bfffc4ec16956a2e6
Holy <khanholy3@gmail.com>
2024-09-05 13:12:33 -0700

    fourth

--- app/src/main/java/com/example/signuplogin/homeFragment.java
+++ app/src/main/java/com/example/signuplogin/homeFragment.java
@@ -2,18 +2,100 @@ package com.example.signuplogin;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class homeFragment extends Fragment {
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home,container,false);
+
+
+        CardView classroutineCard=view.findViewById(R.id.classroutineCard);
+        CardView classCard=view.findViewById(R.id.classCard);
+        CardView waiverCard=view.findViewById(R.id.waiverCard);
+        CardView semesterCard=view.findViewById(R.id.semesterCard);
+        CardView resultCard=view.findViewById(R.id.resultCard);
+        CardView scheduleCard=view.findViewById(R.id.scheduleCard);
+
+
+        classroutineCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RoutineFragment routineFragment=new RoutineFragment();
+                FragmentTransaction transaction=getParentFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container,routineFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+
+            }
+        });
+        classCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AcademicFragment academicFragment=new AcademicFragment();
+                FragmentTransaction transaction=getParentFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container,academicFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+
+            }
+        });
+        waiverCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               WaiverFragment waiverFragment=new WaiverFragment();
+                FragmentTransaction transaction=getParentFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container,waiverFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+
+            }
+        });
+        semesterCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RegistrationFragment registrationFragment=new RegistrationFragment();
+                FragmentTransaction transaction=getParentFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container,registrationFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+
+            }
+        });
+
+        resultCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ResultFragment resultFragment=new ResultFragment();
+                FragmentTransaction transaction=getParentFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container,resultFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+
+            }
+        });
+        scheduleCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ScheduleFragment scheduleFragment=new ScheduleFragment();
+                FragmentTransaction transaction=getParentFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container,scheduleFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+
+            }
+        });
+
+        return view;
     }
 }
